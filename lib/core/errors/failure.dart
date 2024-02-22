609f6dd12355b26e243a7c8ffac309b39faf23c8
MahmoudAlsayes <mahmoudalsayes03@gmail.com>
2024-02-23 01:52:27 +0200

    Create home repo methodes & failure file

---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,3 @@
+abstract class Failures {}
+
+class FeatchFailer extends Failures{}
--- lib/features/home/data/repo/home_repo.dart
+++ lib/features/home/data/repo/home_repo.dart
@@ -1,5 +1,9 @@
-abstract class HomeRepo {
-  fetchBestSellerBook();
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/features/home/data/model/book_model.dart';
+import 'package:dartz/dartz.dart';
 
-  fetchFeaturedBook();
+abstract class HomeRepo {
+  Future<Either< Failures, List<BookModel>>> fetchBestSellerBook();
+
+  Future<Either<Failures ,List<BookModel>>> fetchFeaturedBook();
 }
